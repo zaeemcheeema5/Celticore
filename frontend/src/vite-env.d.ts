3a06391d05f21f62b96a1ce1ce5804c4da579f38
zaeemcheema5 <03-135222-038@student.bahria.edu.pk>
2026-07-25 16:01:37 +0500

    aadd

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -5,3 +5,12 @@ declare module '*.png';
 declare module '*.jpeg';
 declare module '*.webp';
 declare module '*.svg';
+
+interface ImportMetaEnv {
+  readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
+  // add any other VITE_* vars you use here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
